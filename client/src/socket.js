aff6c2aa9202623cd81396db73d8c7f312b0ff83
okan.burcak <okanburcak@gmail.com>
2026-01-12 21:00:37 +0300

    Config: Implement environment-aware backend URL for Dev/Prod

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-// Dev: localhost:3000, Prod: relative
-const ALLOWED_ORIGIN = location.hostname === 'localhost' ? 'http://localhost:3000' : '/';
+// URL determined by VITE_BACKEND_URL env variable
+// Local: http://localhost:3000
+// Prod: http://95.179.160.29:3000
+const URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
 
-export const socket = io(ALLOWED_ORIGIN);
+export const socket = io(URL);
