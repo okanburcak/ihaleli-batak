93bbc6a718136b7f01d9ebeed1cbe551555a5748
okan.burcak <okanburcak@gmail.com>
2026-01-25 07:47:26 +0300

    Remove CORS from Express to rely on Nginx

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors');
+
 const Room = require('./game/Room');
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
 const PORT = 3000;
